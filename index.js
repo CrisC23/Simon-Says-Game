d8c06bbe0536accaa1f5f59f923f8175969454eb
CrisC23 <79122213+CrisC23@users.noreply.github.com>
2021-07-19 00:48:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function makeSound(key) {
       break;
 
       case "s":
-      var tom3 = new Audio('sounds/tom-3.mp3');
+      var tom4 = new Audio('sounds/tom-4.mp3');
       tom3.play();
       break;
       
